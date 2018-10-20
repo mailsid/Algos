9ecc2556c9453daf47a775ca55a04deecef9014f
sidharth <mail.sidsm@gmail.com>
2018-10-20 22:40:50 +0530

    first commit
    Signed-off-by: sidharth <mail.sidsm@gmail.com>

---
+++ com/rbs/sid/arrays/ArrayRotation.java
@@ -0,0 +1,18 @@
+package com.rbs.sid.arrays;
+
+public class ArrayRotation {
+
+
+    public static void main(String[] args){
+
+        int [] input = new int[] {1, 2, 3, 4, 5, 6, 7};
+        int rotateBy = 2;
+
+        rotate(input, rotateBy);
+    }
+
+    private static void rotate(int[] input, int rotateBy) {
+
+
+    }
+}
